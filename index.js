a2dbb7cc4698b053ae32ab389d674e9e26d4610b
andriGo <afomin129@gmail.com>
2022-11-05 16:49:25 +0200

    variables

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function sumOfAreas(squareSide, rectSideA, rectSideB, circleRadius, ellipseMajorAxis, ellipseMinorAxis) {
-	// write your code here
+	const squareArea = squareSide ** 2;
+	const areaOfARectangle = rectSideA * rectSideB;
+	const areaOfACircle = Math.PI * (circleRadius ** 2);
+	const ovalSquare = Math.PI * ellipseMajorAxis * ellipseMinorAxis;
+	const areaOfAllFigures = squareArea + areaOfARectangle + areaOfACircle + ovalSquare;
+	return areaOfAllFigures;
 }
 
 module.exports = sumOfAreas;
