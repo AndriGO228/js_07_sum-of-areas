77523e17e0c219840d500a55d77056ca5909da01
andriGo <afomin129@gmail.com>
2022-11-05 20:15:42 +0200

    one-line

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function sumOfAreas(squareSide, rectSideA, rectSideB, circleRadius, ellipseMajorAxis, ellipseMinorAxis) {
-	// write your code here
+	return squareSide ** 2 + rectSideA * rectSideB + Math.PI * (circleRadius ** 2) + Math.PI * (ellipseMinorAxis * ellipseMajorAxis);
 }
 
 module.exports = sumOfAreas;
